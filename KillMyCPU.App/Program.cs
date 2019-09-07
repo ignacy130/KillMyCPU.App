d7dd4a4b4185f31f613ffe7946ae35c13480d1ee
Ignacy <ignacy130@gmail.com>
2019-09-07 10:59:42 +0200

    build + build config

--- KillMyCPU.App/Program.cs
+++ KillMyCPU.App/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading.Tasks;
 
 namespace KillMyCPU.App
 {
@@ -6,7 +9,54 @@ namespace KillMyCPU.App
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            MainAsync();
+        }
+
+        static private async Task MainAsync()
+        {
+            var random = new Random();
+            while (true)
+            {
+                var actions = new List<Action>();
+                for (int i = 0; i < 3; i++)
+                {
+                    var min = 100000;
+                    var max = 2000000;
+                    actions.Add(async () => await FindPrimeNumber(random.Next(min, max)));
+                }
+                Parallel.Invoke(actions.ToArray());
+            }
+        }
+
+        public static Task<long> FindPrimeNumber(int n)
+        {
+            Console.WriteLine($"Started computing prime number for: {n}");
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+            int count = 0;
+            long a = 2;
+            while (count < n)
+            {
+                long b = 2;
+                int prime = 1;// to check if found a prime
+                while (b * b <= a)
+                {
+                    if (a % b == 0)
+                    {
+                        prime = 0;
+                        break;
+                    }
+                    b++;
+                }
+                if (prime > 0)
+                {
+                    count++;
+                }
+                a++;
+            }
+            stopwatch.Stop();
+            Console.WriteLine(stopwatch.ElapsedMilliseconds);
+            return Task.FromResult(--a);
         }
     }
 }
